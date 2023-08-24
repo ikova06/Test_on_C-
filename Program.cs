40e03abfe0654b4c4efcd9d3a8397f610a5711a8
ikova06 <129886841+ikova06@users.noreply.github.com>
2023-08-24 20:47:49 +0300

    Update Program.cs
    Создали метод string[] ShortArray(string[] array)
    Для сортировки символов из массива. Сначала перебираем элементы массива, а элементы, удовлетворяющее наше условие записываем в новый массив.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿string[] array = {"Hello", "2", "world", ":-)", "Russia", "Denmark", "Kazan" };
 
 int max_word_length = 3;
+
+string[] ShortArray(string[] array)
+{
+    int length = array.Length;
+    string[] result = new string[length];
+    int count = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i].Length <= max_word_length)
+        {
+            result[count] = array[i];
+            count++;
+        }
+    }
+    Array.Resize(ref result, count);
+
+    return result;
+}
