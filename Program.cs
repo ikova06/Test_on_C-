2585f46df04c81ea616cfdcaaa3a312df1eefe2a
ikova06 <129886841+ikova06@users.noreply.github.com>
2023-09-25 23:15:24 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,6 +2,20 @@
 
 int max_word_length = 3;
 
+void PrintArray(string[] array)
+{
+    if (array.Length == 0)
+    {
+        Console.WriteLine("В массиве пусто");
+    }
+    else
+    {
+        Array.ForEach(array, (str) => Console.Write($"{str} "));
+        Console.WriteLine();
+    }
+}
+
+
 string[] ShortArray(string[] array)
 {
     int length = array.Length;
@@ -21,18 +35,7 @@ string[] ShortArray(string[] array)
     return result;
 }
 
-void PrintArray(string[] array)
-{
-    if (array.Length == 0)
-    {
-        Console.WriteLine("В массиве пусто");
-    }
-    else
-    {
-        Array.ForEach(array, (str) => Console.Write($"{str} "));
-        Console.WriteLine();
-    }
-}
+
 
 void PrintTask(string[] array)
  {
